20d86de74590d4813df74c959563444924989587
Alexander Wittmond <awittmond@gmail.com>
2019-10-03 08:15:03 -0500

    updated DetectSingleByteXor

--- haskell/cryptoChallenges/DetectSingleByteXor/Main.hs
+++ haskell/cryptoChallenges/DetectSingleByteXor/Main.hs
@@ -9,15 +9,15 @@ import qualified Data.ByteString as B
 import Debug.Trace
 import Data.Char
 
-csvPath = "/home/alexanderwittmond/code/cryptoChallengesRustHaskell/englishLetterFrequency.csv"
+csvPath = "../../englishFreq2.csv"
 
-stringsPath = "/home/alexanderwittmond/code/cryptoChallengesRustHaskell/4.txt"
+stringsPath = "../../4.txt"
 
 main :: IO ()
 main  = do
   englishLetterFrequency <- loadFrequencyCSV csvPath
   strings <-map (B.unpack . hexToBytes) .  lines <$> readFile stringsPath
-  let matches = filter (all isText) . map (toAscii.snd) $
+  let matches =  map (toAscii.snd) $
          bestMatch (flp 1) englishLetterFrequency (map pure [0..255]) strings
   putStrLn $ head matches
   return ()
